479b2e86243d2ae1405f098f0b1202bd07790d97
Batache Toufic <toufic.batache@etu.sorbonne-universite.fr>
2023-02-08 08:10:26 +0000

    enhancement: Restructuration du code avec un GraphManager

---
+++ candidate.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Candidate:
+    """Data class for keeping track of a candidate."""
+    __label: str
+    __coordinates: tuple
+
+    def __init__(self, label: str, coordinates: tuple):
+        self.__label = label
+        self.__coordinates = coordinates
+
+    def label(self) -> str:
+        """
+        :return: the candidate's label
+        """
+        return self.__label
+
+    def coordinates(self) -> tuple:
+        """
+        :return: the candidate's coordinates
+        """
+        return self.__coordinates
