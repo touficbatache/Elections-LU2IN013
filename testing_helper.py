ccb1b1af721e41c4a253deeaf60ed74916030c58
Batache Toufic <toufic.batache@etu.sorbonne-universite.fr>
2023-02-08 09:58:48 +0100

    testing_helper.py adds a record_time annotation that can be used with any function to record its execution time

---
+++ testing_helper.py
@@ -0,0 +1,11 @@
+from time import monotonic
+
+
+def record_time(function):
+    def wrap(*args, **kwargs):
+        start_time = monotonic()
+        function_return = function(*args, **kwargs)
+        print(f"Run time {monotonic() - start_time} seconds")
+        return function_return
+
+    return wrap
