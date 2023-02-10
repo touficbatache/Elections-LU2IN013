5dd2274bfeb37ad397cff6f0ef4fb711a95fbb32
Batache Toufic <toufic.batache@etu.sorbonne-universite.fr>
2023-02-10 03:40:53 +0100

    Prepare for %1 sprint

---
+++ voting_manager.py
@@ -0,0 +1,8 @@
+class VotingManager:
+    """
+    Managing class for the different voting systems.
+    """
+
+    # TODO: Add the voting you're working on below, as follows:
+    #  def pluralite_simple(...):
+    #    ...
