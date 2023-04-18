88a2390615602f55752d44ae98137c20039f5f74
NehmatH <nehmat_h@hotmail.com>
2023-04-18 14:49:46 +0200

    Add test_candidate.py and test_voter.py

--- tests_package/test_candidate.py
+++ tests_package/test_candidate.py
@@ -6,17 +6,18 @@ class TestCandidate(unittest.TestCase):
     def setUp(self) -> None:
         self.label = "A"
         self.coordinates = (0.2, 0.2)
-        self.candidate = Candidate.random_color(self.label, self.coordinates)
+        self.candidate1 = Candidate.random_color(self.label, self.coordinates)
+        self.candidate2 = Candidate("B", (0.4, 0.4), "xkcd:purple")
 
     def test_candidate_attributes(self):
-        self.assertEqual(self.candidate.get_label(), self.label)
-        self.assertEqual(self.candidate.coordinates(), self.coordinates)
+        self.assertEqual(self.candidate1.get_label(), self.label)
+        self.assertEqual(self.candidate1.coordinates(), self.coordinates)
 
-        self.candidate.set_color("xkcd:purple")
-        self.candidate.set_label("B")
+        self.candidate1.set_color("xkcd:purple")
+        self.candidate1.set_label("B")
 
-        self.assertEqual(self.candidate.get_color(), "xkcd:purple")
-        self.assertEqual(self.candidate.get_label(), "B")
+        self.assertEqual(self.candidate1.get_color(), self.candidate2.get_color())
+        self.assertEqual(self.candidate1.get_label(), self.candidate2.get_label())
 
     def tearDown(self):
         return
