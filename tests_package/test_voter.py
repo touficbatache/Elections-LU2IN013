7761cdf1af9dee6a5a6dbe408d4693ba813ce7a9
NehmatH <nehmat_h@hotmail.com>
2023-04-18 14:49:52 +0200

    Add test_candidate.py and test_voter.py

--- tests_package/test_voter.py
+++ tests_package/test_voter.py
@@ -6,15 +6,16 @@ class TestVoter(unittest.TestCase):
     def setUp(self) -> None:
         self.label = "1"
         self.coordinates = (0.2, 0.2)
-        self.voter = Voter(self.label, self.coordinates)
+        self.voter1 = Voter(self.label, self.coordinates)
+        self.voter2 = Voter("2", (0.4, 0.4))
 
     def test_voter_attributes(self):
-        self.assertEqual(self.voter.get_label(), self.label)
-        self.assertEqual(self.voter.coordinates(), self.coordinates)
+        self.assertEqual(self.voter1.get_label(), self.label)
+        self.assertEqual(self.voter1.coordinates(), self.coordinates)
 
-        self.voter.set_label("2")
-        self.assertEqual(self.voter.get_label(), "2")
-        self.assertNotEqual(self.voter.get_label(), "1")
+        self.voter1.set_label("2")
+        self.assertEqual(self.voter1.get_label(), self.voter2.get_label())
+        self.assertNotEqual(self.voter1.coordinates(), self.voter2.coordinates())
 
     def tearDown(self):
         return
